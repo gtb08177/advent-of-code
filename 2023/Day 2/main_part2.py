977d5988874677b3a11e80c420ee1a0c8512515d
Ryan McNulty <ryan.mcnulty2@gmail.com>
2023-12-04 10:20:40 +0000

    Day 2 - star 2

--- 2023/Day 2/main_part2.py
+++ 2023/Day 2/main_part2.py
@@ -1,5 +1,3 @@
-def is_possible(bag_data):
-    return (bag_data['red'] >= 0 and bag_data['blue'] >= 0 and bag_data['green'] >= 0)
 
 input = open(r"input.txt","r")
 bag_start = {"red":12,"green":13,"blue":14}
@@ -13,20 +11,17 @@ for line in lines:
     game_all_reveals = game_id_and_reveals[1].split('; ') # ['3 blue, 4 red','1 red, 2 green','6 blue; 2 green']
     game_is_possible = True
     
+    minimum_cubes_req = {"red":0,"green":0,"blue":0}
+
     for reveal in game_all_reveals:
         arr_colour_and_count = reveal.split(", ") # ['3 blue', '4 red']
-        running_bag_data = bag_start.copy()
 
         for entry in arr_colour_and_count:
             kv_map = entry.split(" ")
             colour = kv_map[1].strip()
             num_of_colour = int(kv_map[0])
-            running_bag_data[colour] = running_bag_data[colour]-num_of_colour
+            minimum_cubes_req[colour] = max(minimum_cubes_req[colour],num_of_colour)
 
-        if not is_possible(running_bag_data):
-            game_is_possible = False
-    
-    if game_is_possible:
-        running_sum = running_sum + game_id
+    running_sum = running_sum + (minimum_cubes_req["red"] * minimum_cubes_req["green"] * minimum_cubes_req["blue"])
 
 print(running_sum)
